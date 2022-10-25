d32007fd7c7041b4607a0f8a308b74e347c809c1
Eliot <z3st@DESKTOP-S5S5DLR.localdomain>
2022-10-25 12:05:08 +0200

    Installation du panier

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 /////////////////// Variables globales ////////////////////
 var PRODUCT_LIST = [];
 var COLOR_LIST = {};
+var PANIER = [];
 var WHERE = window.location.pathname;
 var ARG = window.location.search;
 //////////////////////// Objects //////////////////////////
@@ -61,6 +62,54 @@ function afficher_tous_articles() {
     document.getElementById("tous-articles").innerHTML = html_article_content;
 }
 
+function afficher_mon_panier()
+{
+  var prix_total = 0;
+  var html_panier_content = `<p>Vous avez ${PANIER.length} articles dans votre panier</p>`;
+  for (var i= 0; i < PANIER.length; i++)
+  {
+    prix_total += PRODUCT_LIST[PANIER[i].id].prix*PANIER[i].quantity;
+    console.log("id",PANIER[i].id);
+    html_panier_content += `
+    <div class="row">
+        <div class="col-6">
+            <img class="small-image" src="${PRODUCT_LIST[PANIER[i].id].img}" alt="Funbox">
+        </div>
+        <div class="col-6">
+            <center>
+            <p>${PRODUCT_LIST[PANIER[i].id].nom}</br><em>couleur - ${PANIER[i].color}</em></p>
+            </center>
+        </div>
+        <div class="col-6">
+            <p>${PRODUCT_LIST[PANIER[i].id].prix}€</p>
+        </div>
+        <div class="col-6">
+            <p>x${PANIER[i].quantity} <span class="badge-green">En stock</span></p>
+            
+        </div>
+        <div class="col-6">
+          <p>${PRODUCT_LIST[PANIER[i].id].prix*PANIER[i].quantity}€</p>
+        </div>
+        <div class="col-6">
+            </br>
+            <a class="mini-button" href="?supprimer-${i}">X</a>
+        </div>
+
+    </div>`;
+  }
+  html_panier_content += `<div class="row">
+    <div class="col-6"></div>
+    <div class="col-6"></div>
+    <div class="col-6"></div>
+    <div class="col-6"></div>
+    <div class="col-6"><h2>Total: ${prix_total}€</h2></div>
+    <div class="col-6"></div>
+  
+    `;
+  document.getElementById("mon-panier").innerHTML = html_panier_content;
+
+}
+
 function afficher_un_article(id) {
   var option_color = `<select id="select-color" onChange="generate_image(this.value,'${PRODUCT_LIST[id].calque1}',this)">`; // Calque0 = Choix de la couleur
   for (const [key, value] of Object.entries(COLOR_LIST)) {
@@ -82,9 +131,10 @@ function afficher_un_article(id) {
               <li><img class="icone" src="img/icone/provenance.png" />Provenance: ${PRODUCT_LIST[id].provenance}</li>
               <li><img class="icone" src="img/icone/prix.png" />Prix: ${PRODUCT_LIST[id].prix}€</li>
           </ul>
-          <p>Couleur: ${option_color}
-          </p>
-          <a href="#" class="link-button mt-150px" onclick="alert('Element ajouté au panier');">Ajouter au panier</a>
+          <p>Couleur: ${option_color}</p>
+          <p>Quantité: <input id="select-quantity" type="number" name"=quantity" value="1"/> </p>
+          <input id="article-id" type="hidden" name="id" value="${PRODUCT_LIST[id].id}"/>
+          <a href="#" class="link-button mt-150px" onclick="treat_panier();">Ajouter au panier</a>
       </div>
       
   </div>
@@ -118,6 +168,57 @@ function generate_image(calque0,calque1,self)
  
 }
 
+
+function getCook(cookiename) 
+  {
+  // Get name followed by anything except a semicolon
+  var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+  // Return everything after the equal sign, or an empty string if the cookie name not found
+  return atob(decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "").replace("@", "="));
+  }
+function setCook(cname, cvalue) 
+{
+    if ('btoa' in window) {
+        cvalue = btoa(cvalue).replace("=", "@");
+    }
+    document.cookie = cname + "=" + cvalue + ";";
+
+    
+}
+function first_generate_cookie()
+{
+  var panier = [];
+  setCook("panier",JSON.stringify(panier))
+  return panier;
+}
+function first_treat_cookie()
+{
+  var panier = JSON.parse(getCook("panier"));
+  return panier;
+}
+
+function treat_panier()
+{
+  var color = document.getElementById('select-color').options[document.getElementById('select-color').selectedIndex].text;
+  var quantity = document.getElementById('select-quantity').value;
+  var article_id = document.getElementById('article-id').value;
+  add_item_panier(article_id,color,quantity);
+}
+function add_item_panier(item_id,item_color = "red",item_quantity = 1)
+{
+  article = {"id":item_id,"color":item_color,"quantity":item_quantity};
+  PANIER.push(article);
+  save_cookie();
+}
+function delete_item_panier(id)
+{
+  PANIER.splice(id, 1);
+  save_cookie();
+}
+function save_cookie()
+{
+  setCook("panier",JSON.stringify(PANIER))
+}
 //////////////////////// Code `onload` //////////////////////////
 let url = 'bdd.json';
 fetch(url)
@@ -146,9 +247,36 @@ fetch(url)
   {
     
   }
+  else if (WHERE == "/panier.html")
+  {
+
+    if (ARG.includes("supprimer-") && ARG.indexOf("supprimer-") == 1)
+    {
+      id = ARG.split("supprimer-")[1];
+      if (!isNaN(id))
+      {
+        id = parseInt(id,10);
+        delete_item_panier(id);
+        document.location.href = "/panier.html";
+      }
+    }
+    afficher_mon_panier();
+  }
   
 })
 .catch(err => { throw err });
+
+// Gestion du panier
+if (getCook("panier") == "")
+{
+  PANIER = first_generate_cookie();
+}
+else
+{
+  PANIER = first_treat_cookie();
+}
+console.log("Panier",PANIER);
+
 
 
 // Format cookie: 
